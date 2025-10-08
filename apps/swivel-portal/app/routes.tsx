0ca966d12815e032598cc189e2506f722dabdc36
nuwan-swivel <nuwan@swivelgroup.com.au>
2025-10-08 16:26:45 +0530

    feat: implemented login flow with office 365

--- apps/swivel-portal/app/routes.tsx
+++ apps/swivel-portal/app/routes.tsx
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
   index('./pages/Dashboard.tsx'),
+  route('auth', './pages/Login.tsx'),
   route('seat-booking', './pages/SeatBooking.tsx'),
 ] satisfies RouteConfig;
